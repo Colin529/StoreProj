2d56ad118454ef281240e39de3d6c8fac8adb586
Colin <weekday-lander.0l@icloud.com>
2025-04-29 16:27:14 -0500

    Update storefront.cpp
    Added user loop

--- storefront.cpp
+++ storefront.cpp
@@ -5,12 +5,6 @@
 #include "product.h"
 #include "shirt.h"
 #include "pants.h"
-#include "socks.h"
-#include "underwear.h"
-#include "shoes.h"
-#include "hat.h"
-
-#include "login.h"
 
 int get_line_count(const std::string& path) {
 	std::ifstream stream;
@@ -31,14 +25,89 @@ int get_line_count(const std::string& path) {
 	return count;
 }
 
-void write() {
-	const std::string path = "Data/userinfo.csv";
+//Logging In, Signing Up, and Creating Users
+std::vector<std::pair<std::string, std::string>> read_database(const std::string& path){
+	std::vector<std::pair<std::string, std::string>> user_database;
+	std::ifstream stream;
+	stream.open(path);
+	
+	if(stream.is_open()){
+		std::string line;
+		std::getline(stream, line); // skip header
+
+		while (std::getline(stream, line)) {
+			std::string username, password;
+			size_t comma_pos = line.find(',');
+			
+			if (comma_pos != std::string::npos) {
+				username = line.substr(0, comma_pos);
+				password = line.substr(comma_pos + 1);
+				user_database.push_back(std::make_pair(username, password));
+			}
+		}
+	}
+	else{
+		std::cout << "FAILED TO OPEN THE FILE!!" << std::endl;
+	}
+
+	stream.close();
+	return user_database;
+}
+
+
+void create_new_user(std::string new_username, std::string new_password, const std::string& path, int count) {
+	std::vector<std::pair<std::string, std::string>> new_user_database = read_database(path); 
+	new_user_database.push_back(std::make_pair(new_username, new_password));
 
 	std::ofstream output;
 	output.open(path);
 
 	if (output.is_open()) {
-		output << "Yo,Check,This,Out,!" << std::endl;
+		output << "username,password\n";
+
+		for(int i = 0; i < new_user_database.size(); i++){
+			output << new_user_database[i].first << "," << new_user_database[i].second << "\n"; 
+		}
+	}
+	else {
+		std::cout << "Failed to Write" << std::endl;
+	}
+
+	output.close();
+}
+
+bool check_login_cred(std::string input_username, std::string input_password, const std::string& path, int count) {
+std::ifstream stream;
+stream.open(path);
+
+	std::string username;
+	std::string password;
+	bool success_flag = false;
+	if (stream.is_open()) {
+		std::string line;
+		std::getline(stream, line);
+	for (int i = 1; i < count; i++) {
+		std::getline(stream, username, ',');
+		std::getline(stream, password);
+		if(input_username == username && input_password == password){
+			success_flag = true;
+			break;
+		}
+	}
+	stream.close();
+	}
+	return success_flag;
+}
+
+//Store based read and write
+void write() {
+	const std::string path = "Data/products.csv";
+
+	std::ofstream output;
+	output.open(path);
+
+	if (output.is_open()) {
+		output << "Yo, ,Check, ,This, ,Out, ,!" << std::endl;
 		std::cout << "Success!";
 	}
 	else {
@@ -75,59 +144,20 @@ product** read_storefront(const std::string& path, int count) {
 			std::getline(stream, line, ',');
 			double rating = stod(line);
 
-			bool char_detected = false;
-			char char_field_one;
-			double num_field_one = 0;
-
 			std::getline(stream, line, ',');
-			std::string temp = line;
-			if (temp.length() == 1) {
-				char new_temp = temp[0];
-				if (new_temp == 'S' || new_temp == 'M' || new_temp == 'L' || new_temp == 'X') {
-					char_field_one = new_temp;
-					char_detected = true;
-				}
-				else {
-					num_field_one = std::stod(temp);
-					char_detected = false;
-				}
-			}
-			else {
-				num_field_one = std::stod(line);
-				char_detected = false;
-			}
-			std::getline(stream, line, ',');
-			double num_field_two = std::stod(line);
+			double num_field_one = std::stod(line);
 
 			std::getline(stream, line);
-			int quantity = std::stoi(line);
+			double num_field_two = std::stod(line);
 
 			//std::getline(stream, line); //no comma because end of line
 			//std::string checked_out = line;
-
-			if (char_detected) {
-				if (clothing_type == "underwear") {
-					storefront[i] = new underwear(clothing_type, brand, price, rating, quantity, char_field_one, num_field_two);
-				}
+			if (clothing_type == "shirt") {
+				storefront[i] = new shirt(clothing_type, brand, price, rating, num_field_one, num_field_two);
 			}
-			else {
-				if (clothing_type == "shirt") {
-					storefront[i] = new shirt(clothing_type, brand, price, rating, quantity, num_field_one, num_field_two);
-				}
-				if (clothing_type == "pants") {
-					storefront[i] = new pants(clothing_type, brand, price, rating, quantity, num_field_one, num_field_two);
-				}
-				if (clothing_type == "socks") {
-					storefront[i] = new socks(clothing_type, brand, price, rating, quantity, num_field_one, num_field_two);
-				}
-				if (clothing_type == "shoes") {
-					storefront[i] = new shoes(clothing_type, brand, price, rating, quantity, num_field_one, num_field_two);
-				}
-				if (clothing_type == "hat") {
-					storefront[i] = new hat(clothing_type, brand, price, rating, quantity, num_field_one, num_field_two);
-				}
+			if (clothing_type == "pants") {
+				storefront[i] = new pants(clothing_type, brand, price, rating, num_field_one, num_field_two);
 			}
-			
 
 		}
 		std::cout << "Successfully read storefront data." << std::endl;
@@ -139,124 +169,120 @@ product** read_storefront(const std::string& path, int count) {
 
 }
 
-login* read_userinfo(const std::string& path, int count) {
-	login* users = new login[count];
-
-	std::ifstream stream;
-	stream.open(path);
-
-	if (stream.is_open()) {
-		std::string line;
-
-		std::getline(stream, line); //skips header row
-
-		for (int i = 0; i < count; i++) {
-
-			std::getline(stream, line, ',');
-			std::string user = line;
-
-			std::getline(stream, line, ',');
-			std::string password = line;
-
-			std::getline(stream, line);
-			char is_admin = line[0];
-
-			users[i] = login(user, password, is_admin);
-
-
-		}
-		std::cout << "Successfully read user information." << std::endl;
-		stream.close();
-	}
-	return users;
-}
-
-login enter_password(login* users, int length, login user) {
-	std::cout << "Please enter your password: ";
-	std::string password;
-	std::getline(std::cin, password);
-	if (password == user.get_password()) {
-		if (user.get_user() == "Karasu") {
-			std::cout << "Welcome, creator! Have fun :D" << std::endl;
-		}
-		else if (user.get_is_admin() == 'Y') {
-			std::cout << "Welcome, Admin " << user.get_user() << "!" << std::endl;
-		}
-		else {
-			std::cout << "Welcome, " << user.get_user() << "!" << std::endl;
-		}
-		return user;
-	}
-	std::cout << "Incorrect password. Please try again." << std::endl;
-	return enter_password(users, length, user);
-}
-//i had to swap these two functions around to get this to work. thanks, c++
-login enter_username(login* users, int length) {
-	std::cout << "Please enter your username: ";
-	std::string username;
-	std::getline(std::cin, username);
-	bool userfound = false;
-	for (int i = 0; i < length; i++) {
-		if (username == users[i].get_user()) {
-			login user = users[i];
-			userfound == true;
-			login confirm = enter_password(users, length, user);
-			return confirm;
-		}
-	}
-	std::cout << "User not found. Please try again." << std::endl;
-	enter_username(users, length);
-}
-
 
 
 
 int main() {
-	const std::string path = "Data/products.csv";
-	int file_length = get_line_count(path)-1;
-
-	const std::string userinfo = "Data/userinfo.csv";
-	int user_count = get_line_count(userinfo) - 1;
-
-	login* users = read_userinfo(userinfo, user_count);
-
-	product** storefront = read_storefront(path, file_length);
-
-	login current_user = enter_username(users, user_count);
-
-	if (current_user.get_user() == "Karasu") {
-		std::cout << "Now printing all user info for super admin level:" << std::endl;
-		for (int i = 0; i < user_count; i++) {
-			if (users[i].get_is_admin() != 'C') {
-				std::cout << "Username: " << users[i].get_user() << " | Password: " << users[i].get_password() << std::endl;
-			}
+	const std::string clothes_path = "Data/products.csv";
+	const std::string PATH = "/Users/colin/Desktop/cpp_final/data/user_database.csv";
+	int file_length = get_line_count(path);
+	
+	//User Loop
+		std::string action_str;
+		char action;
+		bool flag = false;		
+	do{
+		std::cout << "--WELCOME--" << std::endl;
+		std::cout << "Enter 'l' to login or 's' to signup" << std::endl;
+		std::cout << "Enter Command: ";
+		std::getline(std::cin, action_str);
+		action = action_str[0];
+		
+		std::string username;
+		std::string password;
+		switch(action){
+			case 'l':
+				std::cout << "Enter Username: ";
+				std::getline(std::cin, username);
+				std::cout << "Enter Password: ";
+				std::getline(std::cin, password);
+				if (check_login_cred(username, password, user_path, get_line_count(user_path))) {
+					flag = true;
+					std::cout << "Login Successful!" << std::endl;
+				}
+				else{
+					flag = false;
+					std::cout << "THIS USERNAME AND/OR PASSWORD IS NOT RECOGNIZED!" << std::endl;
+				}
+				break;
+				
+			case 's':
+				std::cout << "--SIGNUP--\n" << std::endl;
+				std::cout << "Enter Username: ";
+				std::getline(std::cin, username);
+				std::cout << "Enter Password: ";
+				std::getline(std::cin, password);
+				create_new_user(username, password, user_path, get_line_count(user_path));
+				break;
+				
+			default:
+				std::cout << "Sorry I couldn't find the command " << action << std::endl;
+				break;
 		}
+	}while(!(flag)); //Breaks when login or signup is successful
+
+	flag = false;
+	action_str.clear();
+	action = '\0';
+	
+	do{
+		std::cout << "--WELCOME TO THE STORE FRONT-- \n" << std::endl;
+		std::cout << "a - print all products" << std::endl;
+		std::cout << "b - filter by brand" << std::endl;
+		std::cout << "t - filter by type" << std::endl;
+		std::cout << "r - filter non-reserved clothes" << std::endl;
+		std::cout << "p - filter by price\n" << std::endl;
+		std::cout << "Enter Command: " << std::endl;
+		std::getline(std::cin, action_str);
+		action = action_str[0];
+
+		switch(action){
+			case 'a':
+				std::cout << "all prods" << std::endl;
+				flag = true;
+				break;
+			case 'b': 
+				std::cout << "brands" << std::endl;
+				break;
+			case 't':
+				std::cout << "type" << std::endl;
+				break;
+			case 'r':
+				std::cout << "non-reserved clothes" << std::endl;
+				break;
+			case 'p':
+				std::cout << "filter by price" << std::endl;
+				break;
+			default:
+				std::cout << "Sorry I couldn't find the command  " << action << std::endl;
+				break;
+		}
+
+		
+	}while(!(flag));
+
+
+	return 0;
+}
+
+	/*product* storefront[6]{
+		new shirt("shirt", "Great Value", 38.99, 4.2, 16, 34),
+		new pants("shirt", "Target", 38.99, 4.1, 30, 32),
+		new shirt("shirt", "New brand", 38.99, 4.2, 16, 34),
+		new shirt("shirt", "Other brand", 38.99, 4.2, 16, 34),
+		new shirt("shirt", "This joke", 38.99, 4.2, 16, 34),
+		new shirt("shirt", "Sucks lol", 38.99, 4.2, 16, 34),
+	};*/
+
+	product** storefront = read_storefront(clothes_path, file_length);
+
+	/*for (int i = 0; i < 6; i++) {
+		storefront[i]->print();
 	}
 
-	if (current_user.get_is_admin() == 'Y' || current_user.get_is_admin() == 'C') {
-		std::cout << "Would you like to print out the store's current inventory? Y or N please. ";
-		std::string confirm;
-		std::getline(std::cin, confirm);
-		if (confirm == "Y") {
-			for (int i = 0; i < file_length; i++) {
-				storefront[i]->print();
-			}
-		}
-		else if (confirm == "N") {
-			std::cout << "Alright. Take care, Admin " << current_user.get_user();
-		}
-		else {
-			std::cout << "I didn't quite get that. Please try again later.";
-		}
-	}
-	else {
-		std::cout << "The storefront is currently down. Please check again later.";
-	}
-
-	//CLEAN UP
-	for (int i = 0; i < file_length; i++) {
+	for (int i = 0; i < 6; i++) {
 		delete storefront[i];
-	}
-	//clean up is unecessary for users because it doesnt use new, and the one part that does is deconstructed out of scope
+	}*/
+	
 	return 0;
 }
